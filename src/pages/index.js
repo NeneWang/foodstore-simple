0e59022548e5c87ef019c35fad9b29049ce2b3ff
NelsonWang <wangnelson2@gmail.com>
2021-10-25 02:55:08 -0400

    cleaning | data clean : Todo

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,15 @@ import introductionOneData from "../data/introduction/introductionOne.json";
 import IntroductionTwo from "../components/Sections/Introduction/IntroductionTwo";
 import introductionTwoData from "../data/introduction/introductionTwo.json";
 
+import InstagramOne from "../components/Sections/Instagram/InstagramOne";
+
 export default function homepage1() {
   return (
     <LayoutFour title="Homepage 1" data={sliderData} className="-style-1">
       <SliderTwo data={sliderData} className="-style-1" showDots />
       <IntroductionOne data={introductionOneData} />
       <IntroductionTwo data={introductionTwoData} />
+      <InstagramOne />
     </LayoutFour>
   );
 }
